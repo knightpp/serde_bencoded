e9764ffffcad09c5d369afa743d32000c594fc82
Danil Kondratiev <knightpp@protonmail.com>
2021-01-25 08:56:13 +0200

    represent FileMode as untagged enum

--- examples/torrent.rs
+++ examples/torrent.rs
@@ -25,30 +25,36 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("created by: {:?}", info.created_by);
     println!("encoding: {:?}", info.encoding);
 
-    println!("piece length: {}", info.info.piece_length);
-    println!("pieces (length): {}", info.info.pieces.len());
+    println!(
+        "piece length: {}",
+        bytesize::ByteSize(info.info.piece_length).to_string_as(true)
+    );
+    println!("pieces (count): {}", info.info.pieces.len() as u64);
     println!("private: {:?}", info.info.private);
     println!("name: {}", info.info.name);
-    // display as Single File Mode
-    if let Some(len) = info.info.length {
-        println!("\tSingle File Mode");
-        println!("\tlength: {}", len);
-        println!("\tmd5sum: {:?}", info.info.md5sum);
-    }
-    // display as Multiple File Mode
-    else {
-        println!("\tMultiple File Mode");
-        println!("\tfiles:");
-        for file in info.info.files.unwrap() {
-            println!("\t\tlength: {}", file.length);
-            println!("\t\tmd5sum: {:?}", file.md5sum);
-            println!("\t\tpath: {:?}", file.path);
-            println!();
+    match info.info.mode {
+        // display as Single File Mode
+        FileMode::SingleFile { length, md5sum } => {
+            println!("\tSingle File Mode");
+            println!("\tlength: {}", bytesize::ByteSize(length));
+            println!("\tmd5sum: {:?}", md5sum);
+        }
+        // display as Multiple File Mode
+        FileMode::MultipleFiles { files } => {
+            println!("\tMultiple File Mode");
+            println!("\tfiles:");
+            for file in files {
+                println!("\t\tlength: {}", file.length);
+                println!("\t\tmd5sum: {:?}", file.md5sum);
+                println!("\t\tpath: {:?}", file.path);
+                println!();
+            }
         }
     }
 
     Ok(())
 }
+
 #[derive(Debug, Serialize, Deserialize)]
 struct Info {
     #[serde(rename = "piece length")]
@@ -56,12 +62,22 @@ struct Info {
     pieces: ByteBuf,
     private: Option<i64>,
     name: String,
-    /// if this is `Some` => Single File Mode
-    length: Option<u64>,
-    md5sum: Option<ByteBuf>,
-    /// if this is `Some` => Multiple File mode
-    files: Option<Vec<File>>,
+    #[serde(flatten)]
+    mode: FileMode,
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(untagged)]
+enum FileMode {
+    MultipleFiles {
+        files: Vec<File>,
+    },
+    SingleFile {
+        length: u64,
+        md5sum: Option<ByteBuf>,
+    },
+}
+
 #[derive(Debug, Serialize, Deserialize)]
 struct File {
     length: u64,
